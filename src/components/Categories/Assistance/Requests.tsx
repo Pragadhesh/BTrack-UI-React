ccd7400885b67ba741bb94770f287ac9751612a1
PragadheshG <pragadhesh.gopalakrishnan@gds.ey.com>
2023-02-21 21:11:34 +0530

    Added integration for people and requests

--- src/components/Categories/Assistance/Requests.tsx
+++ src/components/Categories/Assistance/Requests.tsx
@@ -1,5 +1,8 @@
 import { Box, Button, Card, CircularProgress, Modal } from "@mui/material";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../../../constants/backendurl";
+import PeopleList from "../../../interfaces/People";
 
 const modalstyle = {
   position: "absolute" as "absolute",
@@ -20,14 +23,152 @@ function Requests() {
 
   const [open, setOpen] = useState(true);
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const handleClose = () => {
+    setIsAccepted(false);
+    setIsDeleted(false);
+    setOpen(false);
+  };
   const [isDeleted, setIsDeleted] = useState(false);
-  const [isAccepted, setIsAccepted] = useState(true);
-  const assistants = [
-    {
-      name: "pragadhesh",
-    },
-  ];
+  const [isAccepted, setIsAccepted] = useState(false);
+  const [peoplelist, setPeopleList] = useState<PeopleList>([]);
+
+  function acceptRequest(people: any, index: any) {
+    const accept = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.post(
+          `${BACKEND_URL}assistants/accept`,
+          {
+            id: people.id,
+            username: people.user.username,
+            email: people.user.email,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("idToken")}`,
+            },
+          }
+        );
+        peoplelist.splice(index, 1);
+        setIsLoading(false);
+        setIsAccepted(true);
+        handleOpen();
+      } catch (err: any) {
+        if (err.response.status === 401) {
+          console.log("entered this method for refresh");
+          const refreshResponse = await axios.post(
+            `${BACKEND_URL}user/refresh`,
+            null,
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("refreshToken")}`,
+              },
+            }
+          );
+          localStorage.setItem("idToken", refreshResponse.data.idToken);
+          localStorage.setItem(
+            "refreshToken",
+            refreshResponse.data.refreshToken
+          );
+          localStorage.setItem("accessToken", refreshResponse.data.accessToken);
+          accept();
+        } else {
+          console.log(err);
+          setIsLoading(false);
+        }
+      }
+    };
+    accept();
+  }
+
+  function rejectRequest(people: any, index: any) {
+    const accept = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.post(
+          `${BACKEND_URL}assistants/reject`,
+          {
+            id: people.id,
+            username: people.user.username,
+            email: people.user.email,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("idToken")}`,
+            },
+          }
+        );
+        peoplelist.splice(index, 1);
+        setIsLoading(false);
+        setIsDeleted(true);
+        handleOpen();
+      } catch (err: any) {
+        if (err.response.status === 401) {
+          console.log("entered this method for refresh");
+          const refreshResponse = await axios.post(
+            `${BACKEND_URL}user/refresh`,
+            null,
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("refreshToken")}`,
+              },
+            }
+          );
+          localStorage.setItem("idToken", refreshResponse.data.idToken);
+          localStorage.setItem(
+            "refreshToken",
+            refreshResponse.data.refreshToken
+          );
+          localStorage.setItem("accessToken", refreshResponse.data.accessToken);
+          accept();
+        } else {
+          console.log(err);
+          setIsLoading(false);
+        }
+      }
+    };
+    accept();
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(`${BACKEND_URL}assistants/requests`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("idToken")}`,
+          },
+        });
+        setPeopleList(response.data);
+        setIsLoading(false);
+      } catch (err: any) {
+        if (err.response.status === 401) {
+          console.log("entered this method for refresh");
+          const refreshResponse = await axios.post(
+            `${BACKEND_URL}user/refresh`,
+            null,
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("refreshToken")}`,
+              },
+            }
+          );
+          localStorage.setItem("idToken", refreshResponse.data.idToken);
+          localStorage.setItem(
+            "refreshToken",
+            refreshResponse.data.refreshToken
+          );
+          localStorage.setItem("accessToken", refreshResponse.data.accessToken);
+          fetchData();
+        } else {
+          console.log(err);
+          setIsLoading(false);
+        }
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="flex w-full h-full">
       {isLoading && (
@@ -67,37 +208,44 @@ function Requests() {
               </Box>
             </Modal>
           )}
-          {assistants.length == 0 && (
+          {peoplelist.length == 0 && (
             <div className="flex w-full h-full text-4xl text-sky-500 font-dancingscript justify-center items-center">
               No Requests found
             </div>
           )}
-          {assistants.length != 0 && (
+          {peoplelist.length != 0 && (
             <div className="flex flex-col w-full h-full">
               <div className="flex justify-start font-playfair text-xl font-bold text-sky-700 pt-10">
                 Requests
               </div>
               <div className="grid grid-flow-row pt-10 pl-10 gap-5">
-                <Card className="flex w-4/6 h-24 self-center">
-                  <div className="grid grid-cols-2 w-full h-full">
-                    <div className="flex items-center w-full h-full font-dancingscript p-5 text-3xl text-sky-400">
-                      Pragadhesh
-                    </div>
-                    <div className="grid grid-flow-col gap-2 w-full h-full justify-end items-center p-5">
-                      <Button variant="outlined" className=" h-10">
-                        Accept
-                      </Button>
+                {peoplelist.map((people, index) => (
+                  <Card className="flex w-4/6 h-24 self-center" key={index}>
+                    <div className="grid grid-cols-2 w-full h-full">
+                      <div className="flex items-center w-full h-full font-dancingscript p-5 text-3xl text-sky-400">
+                        {people.user.username}
+                      </div>
+                      <div className="grid grid-flow-col gap-2 w-full h-full justify-end items-center p-5">
+                        <Button
+                          variant="outlined"
+                          className=" h-10"
+                          onClick={() => acceptRequest(people, index)}
+                        >
+                          Accept
+                        </Button>
 
-                      <Button
-                        variant="outlined"
-                        color="error"
-                        className=" h-10"
-                      >
-                        Delete
-                      </Button>
+                        <Button
+                          variant="outlined"
+                          color="error"
+                          className=" h-10"
+                          onClick={() => rejectRequest(people, index)}
+                        >
+                          Delete
+                        </Button>
+                      </div>
                     </div>
-                  </div>
-                </Card>
+                  </Card>
+                ))}
               </div>
             </div>
           )}
